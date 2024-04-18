52c70fcff691d1fe4fc7754fb729911aeee1112d
Bilal Cagiran <bcagiran@hotmail.com>
2024-04-18 22:11:17 +0100

    init expo project

---
+++ types.ts
@@ -0,0 +1,29 @@
+export type Product = {
+  id: number;
+  image: string | null;
+  name: string;
+  price: number;
+};
+
+export type PizzaSize = 'S' | 'M' | 'L' | 'XL';
+
+export type OrderStatus = 'New' | 'Cooking' | 'Delivering' | 'Delivered';
+
+export type Order = {
+  id: number;
+  created_at: string;
+  total: number;
+  user_id: string;
+  status: OrderStatus;
+
+  order_items?: OrderItem[];
+};
+
+export type OrderItem = {
+  id: number;
+  product_id: number;
+  products: Product;
+  order_id: number;
+  size: PizzaSize;
+  quantity: number;
+};
