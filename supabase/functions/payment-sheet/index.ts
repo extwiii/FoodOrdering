5c62edece5af52ca7a39a4bb5b79ba3b4e4ec7f8
Bilal Cagiran <bcagiran@hotmail.com>
2024-04-25 01:12:46 +0100

    feat: stripe intent with edge func

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -5,17 +5,34 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 /// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
 
+import { stripe } from '../_utils/stripe.ts'
+
 console.log('Hello from Functions!')
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello world ${name}!`,
-  }
+Deno.serve(async (req: Request) => {
+  try {
+    const { amount } = await req.json()
 
-  return new Response(JSON.stringify(data), {
-    headers: { 'Content-Type': 'application/json' },
-  })
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount,
+      currency: 'usd',
+    })
+
+    const res = {
+      paymentIntent: paymentIntent.client_secret,
+      publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
+    }
+
+    return new Response(JSON.stringify(res), {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } catch (error) {
+    console.log(error)
+    return new Response(JSON.stringify(error), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
 })
 
 /* To invoke locally:
@@ -26,5 +43,5 @@ Deno.serve(async (req) => {
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/payment-sheet' \
     --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
-    --data '{"name":"FoodOrdering"}'
+    --data '{"amount":"1055"}'
 */
